7ac62cbf74614f696bbd5c5af7550e719cb41cd4
PepeRoConde <j.rconde@udc.es>
2025-11-04 21:18:01 +0100

    codigo limpio y entendido! (ver comentario)
    deberias entender todo el codigo por solo mirarlo.
    en main se define una funcion fuera de utils por entorno, que se
    necesita pero la funcion tiene que ser (genomes, config).
    
    para la 2.1 queda:
    - graficas (las hago yo mañana, vibecodeables)
    - ajustar los hiperparametros (me acabo de imprimir el NEAT asi me lo
    voy leyendo)

--- P2/2.1/main.py
+++ P2/2.1/main.py
@@ -5,81 +5,19 @@ import numpy as np
 
 from Entorno import Entorno
 import Plots
-#from utils import evalua_genomas
+from utils import evalua_genoma
 
-with open("P2/2.1/config.yaml", "r") as file: config = yaml.safe_load(file)
+with open("P2/2.1/config.yaml", "r") as file: config_global = yaml.safe_load(file)
 
-pasos_por_episodio = config['pasos_por_episodio']
-alpha1 = config['alpha1']
-alpha2 = config['alpha2']
-alpha3 = config['alpha3']
-sigma = config['sigma']
+entorno = Entorno(pasos_por_episodio=config_global['pasos_por_episodio'], alpha1=config_global['alpha1'],
+                  alpha2=config_global['alpha2'], alpha3=config_global['alpha3'], sigma=config_global['sigma'])
 
-
-entorno = Entorno(
-    pasos_por_episodio=pasos_por_episodio,
-    alpha1=alpha1,
-    alpha2=alpha2,
-    alpha3=alpha3,
-    sigma=sigma
-)
-
-config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                 neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                 'P2/2.1/config.ini')
-
-print(type(config))
-
-poblacion = neat.Population(config)
-#poblacion.add_reporter(neat.StdOutReporter(False))
-
-
-def vectoriza_observacion(obs):
-    return np.concatenate([
-        obs["blob_xy"],
-        obs["IR"],
-        obs["tamano_blob"],
-        obs["velocidad"]
-    ])
-
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, 
+                     neat.DefaultStagnation, 'P2/2.1/config.ini')
 
 def evalua_genomas(genomes, config):
-
-    def vectoriza_observacion(obs):
-        return np.concatenate([
-            obs["blob_xy"],
-            obs["IR"],
-            obs["tamano_blob"],
-            obs["velocidad"]
-        ])
-
-    def evalua_genoma(genoma, config):
-        net = neat.nn.FeedForwardNetwork.create(genoma, config)
-        obs, _ = entorno.reset()
-        obs_vector = vectoriza_observacion(obs)
-    
-        fitness = 0.0
-        done = False
-        paso = 0
-
-        while not done and paso < entorno.pasos_por_episodio:
-            action_raw = net.activate(obs_vector)
-            action = np.clip(action_raw, entorno.velocidad_min, entorno.velocidad_max)
-            obs, recompensa, terminated, truncated, _ = entorno.step(action)
-            obs_vector = vectoriza_observacion(obs)
-            fitness += recompensa
-            done = terminated or truncated
-            paso += 1
-
-        print(f'Fitness {fitness}')
-    
-        return fitness
-    
     for genoma_id, genoma in genomes:
-        print(f"Config type inside evalua_genomas: {type(config)}")
-        genoma.fitness = evalua_genoma(genoma, config)
+        genoma.fitness = evalua_genoma(genoma, config, entorno)
 
-
-
+poblacion = neat.Population(config)
 winner = poblacion.run(evalua_genomas)
-
--- P2/2.1/utils.py
+++ P2/2.1/utils.py
@@ -1,53 +1,30 @@
 import numpy as np
-import neat
-import yaml
+import neat 
 
-from Entorno import Entorno
+def vectoriza_observacion(obs):
+    return np.concatenate([
+        obs["blob_xy"],
+        obs["IR"],
+        obs["tamano_blob"],
+        obs["velocidad"]
+    ])
 
-def evalua_genomas(genomes, config):
+def evalua_genoma(genoma, config, entorno):
+    net = neat.nn.FeedForwardNetwork.create(genoma, config)
+    obs, _ = entorno.reset()
+    obs_vector = vectoriza_observacion(obs)
+    fitness = 0.0
+    done = False
+    paso = 0
 
+    while not done and paso < entorno.pasos_por_episodio:
+        action_raw = net.activate(obs_vector)
+        action = np.clip(action_raw, entorno.velocidad_min, entorno.velocidad_max)
+        obs, recompensa, terminated, truncated, _ = entorno.step(action)
+        obs_vector = vectoriza_observacion(obs)
+        fitness += recompensa
+        done = terminated or truncated
+        paso += 1
 
-    def evalua_genoma(genoma, config, entorno):
-        net = neat.nn.FeedForwardNetwork.create(genoma, config)
-        obs, _ = entorno.reset()
-        obs_vector = flatten_observation(obs)
-    
-        fitness = 0.0
-        done = False
-        paso = 0
-    
-        while not done and paso < entorno.pasos_por_episodio:
-            action_raw = net.activate(obs_vector)
-            action = np.clip(action_raw, entorno.velocidad_min, entorno.velocidad_max)
-            obs, recompensa, terminated, truncated, _ = entorno.step(action)
-            obs_vector = vectoriza_observacion(obs)
-            fitness += recompensa
-            done = terminated or truncated
-            paso += 1
-
-        print(f'Fitness {fitness}')
-    
-        return fitness
-
-        
-    with open("P2/2.1/config.yaml", "r") as file: config = yaml.safe_load(file)
-    
-    pasos_por_episodio = config['pasos_por_episodio']
-    alpha1 = config['alpha1']
-    alpha2 = config['alpha2']
-    alpha3 = config['alpha3']
-    sigma = config['sigma']
-    
-    
-    entorno = Entorno(
-        pasos_por_episodio=pasos_por_episodio,
-        alpha1=alpha1,
-        alpha2=alpha2,
-        alpha3=alpha3,
-        sigma=sigma
-    )
-
-    
-    for genoma_id, genoma in genomes:
-        print(f"Config type inside evalua_genomas: {type(config)}")
-        genoma.fitness = evalua_genoma(genoma, config, entorno)
+    print(f'Fitness {fitness}')
+    return fitness
